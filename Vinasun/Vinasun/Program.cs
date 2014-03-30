32955a929bd2eb276487172694cf5e0adfa3f71d
nqviet733 <nqviet733@gmail.com>
2014-03-30 21:31:15 +0700

    Commit

--- Vinasun/Vinasun/Program.cs
+++ Vinasun/Vinasun/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vinasun.Model;
 using Vinasun.EntityClass;
+using Vinasun.View;
 
 namespace Vinasun
 {
@@ -18,7 +19,7 @@ namespace Vinasun
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new Main());
 
             GroupDTO gm = new GroupDTOImpl();
             EntityDTO em = new EntityDTOImpl();
